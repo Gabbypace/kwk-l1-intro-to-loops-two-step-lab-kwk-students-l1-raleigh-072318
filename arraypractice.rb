cd9c64a9f06fa749eede0e722740914276aad9fd
Gabbypace <golfmarch03@gmail.com>
2018-07-25 15:48:30 +0000

    automatically backed up by learn

--- arraypractice.rb
+++ arraypractice.rb
@@ -2,7 +2,7 @@ newarray = []
 
 # i want to go through numbers 1 to 10 
 # if its an even number put a Im even steven string in that position
-# if its an odd number then put im odd toad in that position
+# if its an odd number then put im odd todd in that position
 #check ourselves after we write it 
 
 counter = 1 
@@ -11,7 +11,7 @@ while counter < 10
     newarray.push("Im even steventebt")
   else 
    counter % 2 == 1 
-    newarray.push("Im odd toad")
+    newarray.push("Im odd todd")
   end
     counter += 1
   end
