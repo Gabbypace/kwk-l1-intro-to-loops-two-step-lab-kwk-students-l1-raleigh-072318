a37bf34f648509086947f5a5cf0abef93ab2c2ba
Gabbypace <golfmarch03@gmail.com>
2018-07-25 15:35:43 +0000

    automatically backed up by learn

--- arraypractice.rb
+++ arraypractice.rb
@@ -1,14 +1,3 @@
-tvshow = []
-tvshow[0] = "Fringe"
-tvshow[1] = "Naruto"
-tvshow[2] = "pokemon"
-tvshow[3] = "the office"
-tvshow[4] = "full house"
-tvshow[5] = "charlies angels"
+÷
 
-# position = 0
-# while position < tvshow.size
-#   puts "#{tvshow[position]}"
-#   position += 1 #position += 1
-# end
 
