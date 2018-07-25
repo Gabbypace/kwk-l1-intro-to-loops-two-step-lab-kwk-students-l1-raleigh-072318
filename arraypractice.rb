cc76f3a7708fbe21d1b5b2430ca6535961edf1b9
Gabbypace <golfmarch03@gmail.com>
2018-07-25 15:38:44 +0000

    automatically backed up by learn

--- arraypractice.rb
+++ arraypractice.rb
@@ -1,3 +1,9 @@
-÷
+newarray = []
+
+# i want to go through numbers 1 to 10 
+# if its an even number put a Im even steven string in that position
+# if its an odd number then put im odd toad in that position
+#check ourselves after we write it 
+
 
 
